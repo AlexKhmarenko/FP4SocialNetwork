25113e50ad0397f2afb6aa1ee339503155a73830
Andrii Horbenko <andrewhorbenko@gmail.com>
2023-04-20 16:36:52 +0300

    Change from app to com.danit.socialnetwork

--- social_network_api/src/main/java/com/danit/socialnetwork/model/User.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/model/User.java
@@ -12,30 +12,30 @@ import java.time.LocalDate;
 @Entity(name = "users")
 @Data
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Integer userId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Integer userId;
 
-    @Column(name = "username")
-    private String username;
+  @Column(name = "username")
+  private String username;
 
-    @Column(name = "password")
-    private String password;
+  @Column(name = "password")
+  private String password;
 
-    @Column(name = "email")
-    private String email;
+  @Column(name = "email")
+  private String email;
 
-    @Column (name = "created_date")
-    private LocalDate createdDate;
+  @Column(name = "created_date")
+  private LocalDate createdDate;
 
-    @Column (name = "name")
-    private String name;
+  @Column(name = "name")
+  private String name;
 
-    @Column (name = "profile_background_image_url")
-    private String profileBackgroundImageURL;
+  @Column(name = "profile_background_image_url")
+  private String profileBackgroundImageUrl;
 
-    @Column(name = "profile_image_url")
-    private String profileImageURL;
+  @Column(name = "profile_image_url")
+  private String profileImageUrl;
 
 }
