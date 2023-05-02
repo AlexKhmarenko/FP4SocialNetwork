5228c565de7a9eedf119303f3bb0adc49ad79098
Oleksandr Khmarenko <khmarenko.a@gmail.com>
2023-05-02 23:34:35 +0300

    Configure Github

---
+++ social_network_api/src/test/java/MyMathTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyMathTest {
+    @Test
+    public void testAdd() {
+//        MyMath math = new MyMath();
+        assertEquals(4, (2+2));
+    }
+
+}
