06194dfde3ed1c3429fee8d02c78f63ab41524d5
Oleksandr Khmarenko <khmarenko.a@gmail.com>
2023-05-07 22:38:56 +0300

    [US13] Create endpoints for Google+ login and forgot password

--- social_network_api/src/main/java/com/danit/socialnetwork/security/WebSecurityConfig.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/security/WebSecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.apache.commons.lang3.StringUtils;
+//import org.apache.commons.lang3.StringUtils;
 
 import java.time.LocalDate;
 import java.util.Arrays;
