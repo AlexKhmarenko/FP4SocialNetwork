4b02be5d45951e449854755c7fcdd5ba0e8f526e
Настя <anastasiiakonyushenko@gmail.com>
2023-05-08 03:33:40 +0300

    US10

--- social-network-ui/src/store/rootReduser.js
+++ social-network-ui/src/store/rootReduser.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
-
+import { stepModalReducer } from "./Reducers/stepModalReducer";
 import { userDataReducer } from "./Reducers/userDataReducer";
 
 const rootReducer = combineReducers({
-    loginUserData: userDataReducer
+    loginUserData: userDataReducer,
+    stepModal: stepModalReducer
 });
 
 export default rootReducer;
