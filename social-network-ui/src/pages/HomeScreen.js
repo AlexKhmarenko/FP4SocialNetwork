23985803bac19de7b606c4db99a235f7c1b346be
Anastasiia Myelnykova <slonotop2103@gmail.com>
2023-05-17 15:44:18 +0300

    add post

--- social-network-ui/src/pages/HomeScreen.js
+++ social-network-ui/src/pages/HomeScreen.js
@@ -1,161 +1,220 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Button, TextField, Box } from "@mui/material";
-import { styled } from "@mui/system";
+import { CloudUploadOutlined } from "@mui/icons-material";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 import { SidebarLogOutButton } from "../components/NavigationComponents/NavigationStyles";
 import { CapybaraSvgPhoto } from "../components/SvgIcons/CapybaraSvgPhoto";
-import { CloudUploadOutlined } from "@mui/icons-material";
 import {
     NameOfUser,
-    PostWrittenInput,
     SvgWrapper,
     WrittenPostWrapper,
     HomeScreenWrapper,
     PostWrapper
 } from "./pagesStyles/HomeScreenStyles";
 import { PostsDisplaying } from "../components/Posts/PostsDisplaying";
-import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
 
 export function HomeScreen() {
     const [postText, setPostText] = useState("");
     const [postImage, setPostImage] = useState(null);
-    const userId = useSelector(state => state.userData.userId);
-
-    const handlePostTextChange = (event) => {
-        setPostText(event.target.value);
-        console.log(postText)
-    };
+    const userId = useSelector(state => state.userData.userData.userId);
 
     const handlePostImageChange = (event) => {
         const file = event.target.files[0];
-        console.log(file)
         setPostImage(file);
     };
 
+    const validationSchema = Yup.object({
+        postText: Yup.string().max(280, "Must be 280 characters or less"),
+    });
 
-    const handlePostSubmit = () => {
-        const formData = new FormData();
-        formData.append("writtenText", postText);
-        formData.append("userId", userId);
-
+    const handlePostSubmit = (values, setSubmitting) => {
+        console.log(values);
         if (postImage) {
             const reader = new FileReader();
+            const formData = new FormData();
+            formData.append("writtenText", postText);
+            formData.append("userId", userId);
 
             reader.onloadend = () => {
                 const imageArrayBuffer = new Uint8Array(reader.result);
-                const imageBlob = new Blob([imageArrayBuffer], { type: "application/octet-stream" });
-                formData.append("photoFile", imageBlob, postImage.name);
-                console.log(FormData)
-                console.log(formData.get("writtenText"));
-                console.log(formData.get("photoFile"));
+                const photoFileByteArray = Array.from(imageArrayBuffer);
 
+                const postObject = {
+                    writtenText: values.postText,
+                    photoFileByteArray: photoFileByteArray,
+                    userId: userId
+                };
+
+                console.log(postObject);
+                setSubmitting(true);
                 fetch("http://localhost:8080/posts", {
                     method: "POST",
-                    body: formData,
-                })
-                    .then((response) => response.json())
-                    .then((data) => {
-                        // Обработка ответа от сервера
-                        console.log(data);
-                        // Очистить поля после отправки
-                        setPostText("");
-                        setPostImage(null);
-                    })
-                    .catch((error) => {
-                        // Обработка ошибки
-                        console.error(error);
-                    });
+                    body: JSON.stringify(postObject),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }).then((response) => {
+                    console.log("vse gud");
+                    setPostText("");
+                    setPostImage(null);
+                }).catch((error) => {
+                    console.log("Pizda");
+                    console.error(error);
+                }).finally(() => {
+                    setSubmitting(false);
+                });
             };
-
             reader.readAsArrayBuffer(postImage);
         }
     };
 
+    const CustomTextField = ({ field, form, ...props }) => {
+        return (
+            <TextField
+                inputProps={{
+                    maxLength: 280,
+                }}
+                {...field}
+                {...props}
+                variant="standard"
+                multiline
+                helperText={form.errors[field.name] && form.touched[field.name] ? form.errors[field.name] : ""}
+            />
+        );
+    };
+
     return (
-        <>
-            <div style={HomeScreenWrapper}>
-                <div style={PostWrapper}>
-                    <div style={SvgWrapper}>
-                        <CapybaraSvgPhoto />
-                    </div>
-                    <div style={WrittenPostWrapper}>
-                        <h2 style={NameOfUser}>Capybara name</h2>
-                        <TextField
-                            multiline
-                            value={postText}
-                            onChange={handlePostTextChange}
-                            InputLabelProps={{
-                                sx: {
-                                    fontSize: "1.3rem",
-                                    fontFamily: "'Lato', sans-serif",
-                                },
-                            }}
-                            sx={{ width: "400px", maxWidth: "600px" }}
-                            id="standard-basic"
-                            label="What's happening?"
-                            variant="standard"
-                        />
-                        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "10px" }}>
-                            {postImage && (
-                                <img
-                                    src={URL.createObjectURL(postImage)}
-                                    alt="Post Image"
-                                    style={{ maxWidth: "100%", height: "auto" }}
+        <Formik
+            initialValues={{ postText: "" }}
+            validationSchema={validationSchema}
+            onSubmit={(values, { resetForm, setSubmitting }) => {
+                setSubmitting(true);
+                handlePostSubmit(values, setSubmitting);
+                resetForm();
+            }}
+        >
+            {({ values, errors, touched, isSubmitting }) => (
+                <Form>
+                    <div style={HomeScreenWrapper}>
+                        <div style={PostWrapper}>
+                            <div style={SvgWrapper}>
+                                <CapybaraSvgPhoto/>
+                            </div>
+                            <div style={WrittenPostWrapper}>
+                                <h2 style={NameOfUser}>Capybara name</h2>
+                                <Field
+                                    values={postText}
+                                    component={CustomTextField}
+                                    name="postText"
+                                    className={errors.postText && touched.postText ? "error" : ""}
+                                    style={{
+                                        fontSize: "1.3rem",
+                                        fontFamily: "'Lato', sans-serif",
+                                        width: "400px",
+                                        maxWidth: "600px",
+                                        marginTop: "20px",
+                                    }}
+                                    id="postText"
+                                    placeholder="What's happening?"
                                 />
-                            )}
-                            <input
-                                type="file"
-                                accept="image/*"
-                                id="post-image-input"
-                                onChange={handlePostImageChange}
-                                style={{ display: "none" }}
-                            />
-                            <label htmlFor="post-image-input">
-                                <div style={{display:"flex", justifyContent:"space-between", maxWidth:"400px",width:"400px"}}>
-                                <Button
-                                    component="span"
-                                    variant="contained"
-                                    color="primary"
-                                    sx={{
-                                        ...SidebarLogOutButton,
-                                        color: "white",
-                                        marginRight: "10px",
-                                        alignSelf: "end",
-                                        marginTop: "30px",
-                                        fontFamily: "'Lato', sans-serif",
-                                        fontSize: "14px",
-                                        cursor: "pointer",
-                                    }}
-                                    startIcon={<CloudUploadOutlined />}
-                                >
-                                   image
-                                </Button>
-                                <Button
-                                    type="submit"
-                                    variant="contained"
-                                    sx={{
-                                        ...SidebarLogOutButton,
-                                        alignSelf: "end",
-                                        marginTop: "30px",
-                                        fontFamily: "'Lato', sans-serif",
-                                    }}
-                                    fullWidth={true}
-                                    onClick={handlePostSubmit}
-                                >
-                                    Post
-                                </Button>
+                                <div style={{
+                                    marginTop: "10px", fontSize: "1rem",
+                                    fontFamily: "'Lato', sans-serif",
+                                }}>
+                                    {
+                                        280 - values.postText.length >= 0 ?
+                                            (280 - values.postText.length + " characters") : ("maximum number of characters 280")
+                                    }
                                 </div>
-                            </label>
-
-
-                        </Box>
+                                <Box sx={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    alignItems: "center",
+                                    marginTop: "10px",
+                                }}>
+                                    {postImage && (
+                                        <img
+                                            src={URL.createObjectURL(postImage)}
+                                            alt="Post Image"
+                                            style={{ maxWidth: "100%", height: "auto" }}
+                                        />
+                                    )}
+                                    <input
+                                        type="file"
+                                        accept="image/*"
+                                        id="post-image-input"
+                                        onChange={handlePostImageChange}
+                                        style={{ display: "none" }}
+                                    />
+                                    <label htmlFor="post-image-input">
+                                        <div
+                                            style={{
+                                                display: "flex",
+                                                justifyContent: "space-between",
+                                                maxWidth: "400px",
+                                                width: "400px",
+                                            }}
+                                        >
+                                            <Button
+                                                component="span"
+                                                variant="contained"
+                                                color="primary"
+                                                sx={{
+                                                    ...SidebarLogOutButton,
+                                                    color: "white",
+                                                    marginRight: "10px",
+                                                    alignSelf: "end",
+                                                    marginTop: "30px",
+                                                    fontFamily: "'Lato', sans-serif",
+                                                    fontSize: "14px",
+                                                    cursor: "pointer",
+                                                }}
+                                                startIcon={<CloudUploadOutlined/>}
+                                                disabled={!!postImage}
+                                            >image</Button>
+                                            <Button
+                                                type="submit"
+                                                variant="contained"
+                                                sx={{
+                                                    ...SidebarLogOutButton,
+                                                    alignSelf: "end",
+                                                    marginTop: "30px",
+                                                    fontFamily: "'Lato', sans-serif",
+                                                }}
+                                                fullWidth={true}
+                                                disabled={isSubmitting}
+                                            >
+                                                {isSubmitting ? "Posting..." : "Post"}
+                                            </Button>
+                                        </div>
+                                    </label>
+                                </Box>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        maxWidth: "100%",
+                    }}>
+                        <PostsDisplaying/>
+                    </div>
+                </Form>
+            )}
+        </Formik>
 
-
-            </div>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", maxWidth: "100%" }}>
-                <PostsDisplaying />
-            </div>
-        </>
     );
 }
+
+HomeScreen.propTypes = {
+    field: PropTypes.string,
+    form: PropTypes.string,
+    type: PropTypes.string,
+};
+
+
