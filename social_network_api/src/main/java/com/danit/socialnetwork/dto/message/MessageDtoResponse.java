2b4d8070ea5b0112340fbc35444f90232664a9fb
BukanNadya <118114796+BukanNadya@users.noreply.github.com>
2023-05-19 14:52:57 +0300

    [US34] Add class 'MessageDtoUnitTest' and tests for Dto responses.

--- social_network_api/src/main/java/com/danit/socialnetwork/dto/message/MessageDtoResponse.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/dto/message/MessageDtoResponse.java
@@ -4,20 +4,22 @@ import com.danit.socialnetwork.model.Message;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 public class MessageDtoResponse {
-  Integer messageId;
   private Integer inboxUid;
   private Integer userId;
   private String writtenMessage;
+  private LocalDateTime createdAt;
 
   public static MessageDtoResponse from(Message message) {
     MessageDtoResponse messageDto = new MessageDtoResponse();
-    messageDto.setMessageId(message.getMessageId());
     messageDto.setInboxUid(message.getInboxUid());
     messageDto.setUserId(message.getUserId());
     messageDto.setWrittenMessage(message.getMessage());
+    messageDto.setCreatedAt(message.getCreatedAt());
     return messageDto;
   }
 }
