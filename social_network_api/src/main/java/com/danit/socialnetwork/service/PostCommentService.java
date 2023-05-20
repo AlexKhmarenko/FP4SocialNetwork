64f51d99281c6ceb716fa4585b36a5901d2a6f57
Andrii Horbenko <andrewhorbenko@gmail.com>
2023-05-20 17:50:46 +0300

    US 37 remove codesmell

--- social_network_api/src/main/java/com/danit/socialnetwork/service/PostCommentService.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/service/PostCommentService.java
@@ -2,7 +2,6 @@ package com.danit.socialnetwork.service;
 
 import com.danit.socialnetwork.dto.post.PostCommentDtoSave;
 import com.danit.socialnetwork.model.PostComment;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
