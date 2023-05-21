aea11411e89fb16faf93982785babc1f9f7b23bd
AlexKhmarenko <106476677+AlexKhmarenko@users.noreply.github.com>
2023-05-21 23:15:41 +0300

    Update WebSecurityConfig.java

--- social_network_api/src/main/java/com/danit/socialnetwork/security/WebSecurityConfig.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/security/WebSecurityConfig.java
@@ -1,128 +1,52 @@
 package com.danit.socialnetwork.security;
 
-//import com.danit.socialnetwork.config.CorsFilter;
-
 import com.danit.socialnetwork.config.GuavaCache;
 import com.danit.socialnetwork.model.DbUser;
 import com.danit.socialnetwork.repository.UserRepository;
 import com.ibm.icu.text.Transliterator;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+Expand All
+	@@ -15,6 +20,8 @@
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.time.LocalDate;
 import java.util.Optional;
-import java.util.UUID;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-@Log4j2
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-  private final AuthenticationEntryPoint jwtAuthenticationEntryPoint;
-  private final JwtAuthFilter jwtFilter;
-  private final UserRepository userRepository;
-
-  @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
-
-  @Bean
-  GuavaCache guavaCache() {
+Expand All
+	@@ -40,10 +47,16 @@ GuavaCache guavaCache() {
     return new GuavaCache();
   }
-
-  @Autowired
-  private CorsFilter corsFilter;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
     final String[] userName = new String[1];
-    http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-    ;
 
     http.csrf().disable().headers().frameOptions().disable();
 
-    http.authorizeRequests().antMatchers("/login",
-            "/h2/**",
-            "/sendLetter",
-            "/activate",
-            "/checkEmail",
-            "/registration",
-            "/changepassword*",
-            "/newpassword",
-            "/home",
-            "/",
-            "/**").permitAll()
-        .antMatchers()
-        .authenticated()
-        .anyRequest()
-        .authenticated();
-    http.oauth2Login(l -> l
-        .successHandler((request, response, authentication) -> {
-          OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
-
-          userName[0] = oauth2User.getAttributes().get("given_name").toString();
-          String userSurname = oauth2User.getAttributes().get("family_name").toString();
-          String userMail = oauth2User.getAttributes().get("email").toString();
-
-          log.info("User with username {} has logged in", userName[0]);
-          log.info("User name - {}", userName[0]);
-          log.info("User surname - {}", userSurname);
-          log.info("User e-mail - {}", userMail);
-          Optional<DbUser> maybeUser = userRepository.findDbUserByEmail(userMail);
-          if (maybeUser.isEmpty()) {
-
-            Transliterator transliteratorUa = Transliterator.getInstance("Ukrainian-Latin/BGN");
-            Transliterator transliteratorRu = Transliterator.getInstance("Russian-Latin/BGN");
-            String latinName = transliteratorUa.transliterate(userName[0]);
-            latinName = transliteratorRu.transliterate(latinName);
-
-            DbUser newUser = new DbUser(latinName,
-                UUID.randomUUID().toString(), userMail,
+Expand Down
+Expand Up
+	@@ -87,7 +100,7 @@ protected void configure(HttpSecurity http) throws Exception {
                 userName[0], LocalDate.of(1900, 1, 1));
             userRepository.save(newUser);
           }
-          response.sendRedirect("http://localhost:3000/home");
+          response.sendRedirect("/home");
         }).permitAll()
     );
     http.logout(l -> l
-        .logoutSuccessHandler((request, response, authentication) -> {
-          log.info("User with username {} has logged out", userName[0]);
-          //                    response.sendRedirect("/oauth2/authorization/google");
-          response.sendRedirect("/login");
-        })
-        .clearAuthentication(true)
-        .invalidateHttpSession(true)
-        .deleteCookies("JSESSIONID")
-        .deleteCookies("XSRF-TOKEN").permitAll());
-
-
+Expand All
+	@@ -105,9 +118,11 @@ protected void configure(HttpSecurity http) throws Exception {
     http.rememberMe();
 
     http.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
+    //    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    //
+    http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
   }
+
 }
-
-
-
-
-
-
