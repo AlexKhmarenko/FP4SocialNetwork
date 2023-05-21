380afefdf61ced89325ddaf7341ab87f12b45a7b
AndriiHorbenko <118114091+AndriiHorbenko@users.noreply.github.com>
2023-05-21 10:41:46 +0300

    US22.3 updates endpoints for posts and clean test files (#173)
    * Correction and improvements with changes to pom.xml
    
    * US 22.3 update posts with receiving posts and clean pom.xml and tests
    
    * US 22.3 clean code smells

--- social_network_api/src/test/java/com/danit/socialnetwork/service/PostServiceImplTest.java
+++ social_network_api/src/test/java/com/danit/socialnetwork/service/PostServiceImplTest.java
@@ -6,20 +6,15 @@ import com.danit.socialnetwork.dto.post.PostDtoSave;
 import com.danit.socialnetwork.model.DbUser;
 import com.danit.socialnetwork.model.Post;
 import com.danit.socialnetwork.model.PostComment;
-import com.danit.socialnetwork.model.PostLike;
-import com.danit.socialnetwork.model.UserFollow;
+import com.danit.socialnetwork.model.UserFollower;
 import com.danit.socialnetwork.repository.PostRepository;
-import com.danit.socialnetwork.repository.UserFollowRepository;
 import com.danit.socialnetwork.repository.UserRepository;
-import com.danit.socialnetwork.service.PostService;
-import com.danit.socialnetwork.service.PostServiceImpl;
-import junit.framework.TestCase;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -36,22 +31,14 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 
-@RunWith(MockitoJUnitRunner.class)
-public class PostServiceImplTest extends TestCase {
-
+@ExtendWith(MockitoExtension.class)
+public class PostServiceImplTest {
+  @InjectMocks
+  PostServiceImpl postService;
   @Mock
   PostRepository postRepository;
   @Mock
   UserRepository userRepository;
-  @Mock
-  UserFollowRepository userFollowRepository;
-  @Mock
-  PostService postService;
-
-  @Before
-  public void setUp() {
-    postService = new PostServiceImpl(postRepository, userFollowRepository, userRepository);
-  }
 
   @Test
   public void testGetAllPostsFromToFollow() {
@@ -71,15 +58,15 @@ public class PostServiceImplTest extends TestCase {
     user2.setUsername("Jim");
     user2.setName("Jimmy");
 
-    UserFollow userFollow1 = new UserFollow();
-    userFollow1.setUserFollowId(1);
-    userFollow1.setUserFollowerId(user.getUserId());
-    userFollow1.setUserFollowingId(user1.getUserId());
+    UserFollower userFollower1 = new UserFollower();
+    userFollower1.setUserFollowId(1);
+    userFollower1.setUserFollowerId(user);
+    userFollower1.setUserFollowingId(user1);
 
-    UserFollow userFollow2 = new UserFollow();
-    userFollow2.setUserFollowId(2);
-    userFollow2.setUserFollowerId(user.getUserId());
-    userFollow2.setUserFollowingId(user2.getUserId());
+    UserFollower userFollower2 = new UserFollower();
+    userFollower2.setUserFollowId(2);
+    userFollower2.setUserFollowerId(user);
+    userFollower2.setUserFollowingId(user2);
 
     Post post1 = new Post();
     post1.setPostId(1);
@@ -88,8 +75,7 @@ public class PostServiceImplTest extends TestCase {
     post1.setPhotoFile("MTA6MjQ6MjY=");
     LocalDateTime dateTime = LocalDateTime.now();
     post1.setSentDateTime(dateTime);
-    post1.setPostLikes(new ArrayList<PostLike>() {
-    });
+
     post1.setPostComments(new ArrayList<PostComment>() {
     });
 
@@ -100,8 +86,7 @@ public class PostServiceImplTest extends TestCase {
     post2.setPhotoFile("MTA6MjQ6MjY=");
     LocalDateTime dateTime2 = LocalDateTime.now();
     post2.setSentDateTime(dateTime2);
-    post2.setPostLikes(new ArrayList<PostLike>() {
-    });
+
     post2.setPostComments(new ArrayList<PostComment>() {
     });
 
@@ -109,7 +94,7 @@ public class PostServiceImplTest extends TestCase {
     Pageable pagedByFivePosts =
         PageRequest.of(0, 12);
 
-    List<UserFollow> userFollowList = new ArrayList<>(Arrays.asList(userFollow1, userFollow2));
+    List<UserFollower> userFollowerList = new ArrayList<>(Arrays.asList(userFollower1, userFollower2));
 
     when(postRepository.findAllPostsFromToFollow(user.getUserId(), pagedByFivePosts)).thenReturn(postList);
 
@@ -135,9 +120,7 @@ public class PostServiceImplTest extends TestCase {
     post1.setPhotoFile("MTA6MjQ6MjY=");
     LocalDateTime dateTime = LocalDateTime.now();
     post1.setSentDateTime(dateTime);
-    post1.setPostLikes(new ArrayList<PostLike>() {
-    });
-    post1.setPostComments(new ArrayList<PostComment>() {
+    post1.setPostComments(new ArrayList<>() {
     });
 
     Post post2 = new Post();
@@ -150,9 +133,8 @@ public class PostServiceImplTest extends TestCase {
     post2.setPhotoFile("MTA6MjQ6MjY=");
     LocalDateTime dateTime2 = LocalDateTime.now();
     post2.setSentDateTime(dateTime2);
-    post2.setPostLikes(new ArrayList<PostLike>() {
-    });
-    post2.setPostComments(new ArrayList<PostComment>() {
+
+    post2.setPostComments(new ArrayList<>() {
     });
 
     Pageable sortedByDateTimeDesc =
@@ -161,10 +143,8 @@ public class PostServiceImplTest extends TestCase {
     List<Post> postList = new ArrayList<>(Arrays.asList(post1, post2));
     Page<Post> pagePost = new PageImpl<>(postList);
 
-    System.out.println(postList);
     when(postRepository.findAll(sortedByDateTimeDesc)).thenReturn(pagePost);
     List<PostDtoResponse> result = postService.getAllPosts(0);
-    System.out.println(result);
     Assertions.assertEquals(result.get(0).getWrittenText(), post1.getWrittenText());
     Assertions.assertEquals(result.get(1).getName(), post2.getUserPost().getName());
     Assertions.assertEquals(2, result.toArray().length);
@@ -187,16 +167,57 @@ public class PostServiceImplTest extends TestCase {
     Post tempPost = Post.from(postDtoSave, user);
 
     when(userRepository.findById(postDtoSave.getUserId())).thenReturn(Optional.of(user));
-
     when(postRepository.save(any(Post.class))).thenReturn(tempPost);
 
-
     Post post = postService.savePost(postDtoSave);
-    System.out.println(post);
 
     Assertions.assertEquals(post.getWrittenText(), postDtoSave.getWrittenText());
     Assertions.assertEquals(post.getUserPost().getUsername(), user.getUsername());
 
+  }
+
+  @Test
+  void getAllOwnPosts() {
+
+    Integer userId = 1;
+
+    DbUser user = new DbUser();
+    user.setUserId(userId);
+    user.setUsername("John1");
+    user.setName("Johny1");
+
+    Post post1 = new Post();
+    post1.setPostId(1);
+    post1.setUserPost(user);
+    post1.setWrittenText("Hello world1");
+    post1.setPhotoFile("MTA6MjQ6MjY=");
+    LocalDateTime dateTime = LocalDateTime.now();
+    post1.setSentDateTime(dateTime);
+
+    post1.setPostComments(new ArrayList<PostComment>() {
+    });
+
+    Post post2 = new Post();
+    post2.setPostId(2);
+    post2.setUserPost(user);
+    post2.setWrittenText("Hello world2");
+    post2.setPhotoFile("MTA6MjQ6MjY=");
+    LocalDateTime dateTime2 = LocalDateTime.now();
+    post2.setSentDateTime(dateTime2);
+
+    post2.setPostComments(new ArrayList<PostComment>() {
+    });
+
+    List<Post> postList = Arrays.asList(post1, post2);
+    Pageable pagedByTenPosts =
+        PageRequest.of(0, 10);
+
+    when(postRepository.findAllByUserId(user.getUserId(), pagedByTenPosts)).thenReturn(postList);
+    List<PostDtoResponse> result = postService.getAllOwnPosts(userId, 0);
+
+    Assertions.assertEquals(result.get(0).getWrittenText(), post1.getWrittenText());
+    Assertions.assertEquals(result.get(1).getName(), post2.getUserPost().getName());
+    Assertions.assertEquals(2, result.toArray().length);
 
   }
 }
