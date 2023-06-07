bd7f151cfa01b9380a4b1fd2c5eae1e1c499d89f
Oleksandr Khmarenko <khmarenko.a@gmail.com>
2023-06-07 14:50:15 +0300

    final commit

--- social_network_api/src/main/java/com/danit/socialnetwork/security/WebSecurityConfig.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/security/WebSecurityConfig.java
@@ -29,103 +29,103 @@ import java.util.UUID;
 @Log4j2
 @PropertySource("classpath:deploy.properties")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtAuthFilter jwtFilter;
-    private final UserRepository userRepository;
-    private final JwtTokenService jwtTokenService;
-    @Value("${uiUrl}")
-    private String uiUrl;
+  private final AuthenticationEntryPoint jwtAuthenticationEntryPoint;
+  private final JwtAuthFilter jwtFilter;
+  private final UserRepository userRepository;
+  private final JwtTokenService jwtTokenService;
+  @Value("${uiUrl}")
+  private String uiUrl;
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
 
-    @Bean
-    GuavaCache guavaCache() {
-        return new GuavaCache();
-    }
+  @Bean
+  GuavaCache guavaCache() {
+    return new GuavaCache();
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
 
-        final String[] userName = new String[1];
+    final String[] userName = new String[1];
 
-        http.csrf().disable().headers().frameOptions().disable();
+    http.csrf().disable().headers().frameOptions().disable();
 
-        http.authorizeRequests().antMatchers("/login",
-                        "/h2/**",
-                        "/sendLetter",
-                        "/activate",
-                        "/checkEmail",
-                        "/registration",
-                        "/changepassword*",
-                        "/newpassword",
-                        "/home",
-                        "/",
-                        "/**").permitAll()
-                .antMatchers()
-                .authenticated()
-                .anyRequest()
-                .authenticated();
-        http.oauth2Login(l -> l
-                .loginPage("/oauth2/google")
-                .successHandler((request, response, authentication) -> {
-                    OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+    http.authorizeRequests().antMatchers("/login",
+            "/h2/**",
+            "/sendLetter",
+            "/activate",
+            "/checkEmail",
+            "/registration",
+            "/changepassword*",
+            "/newpassword",
+            "/home",
+            "/",
+            "/**").permitAll()
+        .antMatchers()
+        .authenticated()
+        .anyRequest()
+        .authenticated();
+    http.oauth2Login(l -> l
+        .loginPage("/oauth2/google")
+        .successHandler((request, response, authentication) -> {
+          OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
 
-                    userName[0] = oauth2User.getAttributes().get("given_name").toString();
-                    String userSurname = oauth2User.getAttributes().get("family_name").toString();
-                    String userMail = oauth2User.getAttributes().get("email").toString();
+          userName[0] = oauth2User.getAttributes().get("given_name").toString();
+          String userSurname = oauth2User.getAttributes().get("family_name").toString();
+          String userMail = oauth2User.getAttributes().get("email").toString();
 
-                    log.info("User with username {} has logged in", userName[0]);
-                    log.info("User name - {}", userName[0]);
-                    log.info("User surname - {}", userSurname);
-                    log.info("User e-mail - {}", userMail);
-                    Optional<DbUser> maybeUser = userRepository.findDbUserByEmail(userMail);
-                    Integer userId = 0;
-                    String birthday = "false";
+          log.info("User with username {} has logged in", userName[0]);
+          log.info("User name - {}", userName[0]);
+          log.info("User surname - {}", userSurname);
+          log.info("User e-mail - {}", userMail);
+          Optional<DbUser> maybeUser = userRepository.findDbUserByEmail(userMail);
+          Integer userId = 0;
+          String birthday = "false";
 
-                    if (maybeUser.isEmpty()) {
-                        Transliterator transliteratorUa = Transliterator.getInstance("Ukrainian-Latin/BGN");
-                        Transliterator transliteratorRu = Transliterator.getInstance("Russian-Latin/BGN");
-                        String latinName = transliteratorUa.transliterate(userName[0]);
-                        latinName = transliteratorRu.transliterate(latinName);
-                        DbUser newUser = new DbUser(latinName,
-                                UUID.randomUUID().toString(), userMail,
-                                userName[0], LocalDate.of(1900, 1, 1));
-                        userRepository.save(newUser);
-                    } else {
-                        userId = maybeUser.get().getUserId();
-                        LocalDate dob = maybeUser.get().getDateOfBirth();
-                        if (!dob.isEqual(LocalDate.of(1900, 1, 1))) {
-                            birthday = "true";
-                        }
-                    }
-                    String token = jwtTokenService.generateToken(userId, true);
-                    log.info("token:" + token);
+          if (maybeUser.isEmpty()) {
+            Transliterator transliteratorUa = Transliterator.getInstance("Ukrainian-Latin/BGN");
+            Transliterator transliteratorRu = Transliterator.getInstance("Russian-Latin/BGN");
+            String latinName = transliteratorUa.transliterate(userName[0]);
+            latinName = transliteratorRu.transliterate(latinName);
+            DbUser newUser = new DbUser(latinName,
+                UUID.randomUUID().toString(), userMail,
+                userName[0], LocalDate.of(1900, 1, 1));
+            userRepository.save(newUser);
+          } else {
+            userId = maybeUser.get().getUserId();
+            LocalDate dob = maybeUser.get().getDateOfBirth();
+            if (!dob.isEqual(LocalDate.of(1900, 1, 1))) {
+              birthday = "true";
+            }
+          }
+          String token = jwtTokenService.generateToken(userId, true);
+          log.info("token:" + token);
 
-                    response.sendRedirect(uiUrl + "?token=" + token + "&" + "birthday=" + birthday);
-                }).permitAll()
+          response.sendRedirect(uiUrl + "?token=" + token + "&" + "birthday=" + birthday);
+        }).permitAll()
 
-        );
-        http.logout(l -> l
-                .logoutSuccessHandler((request, response, authentication) -> {
-                    log.info("User with username {} has logged out", userName[0]);
-                    response.sendRedirect("/login");
-                })
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .deleteCookies("XSRF-TOKEN").permitAll());
+    );
+    http.logout(l -> l
+        .logoutSuccessHandler((request, response, authentication) -> {
+          log.info("User with username {} has logged out", userName[0]);
+          response.sendRedirect("/login");
+        })
+        .clearAuthentication(true)
+        .invalidateHttpSession(true)
+        .deleteCookies("JSESSIONID")
+        .deleteCookies("XSRF-TOKEN").permitAll());
 
 
-        http.rememberMe();
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.NEVER);
+    http.rememberMe();
+    http.sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.NEVER);
 
-        http.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-    }
+    http.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
+    http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+  }
 
 }
