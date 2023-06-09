f57ffe244f3b7d057ec9ea91209eba6ae4a89990
Настя <anastasiiakonyushenko@gmail.com>
2023-06-09 18:02:47 +0300

    verstka

--- social-network-ui/src/components/Layout.js
+++ social-network-ui/src/components/Layout.js
@@ -2,120 +2,154 @@ import React, { useEffect, useCallback, useState } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import { Container, } from "@mui/material";
+import { Container } from "@mui/material";
 
 import { HeaderInformation } from "./NavigationComponents/HeaderInformation";
 import { UsersSearch } from "./NavigationComponents/UsersSearch/UsersSearch";
 import { SideBar } from "./NavigationComponents/SideBar";
 import {
-    ContainerStyled,
-    ContentContainer,
-    ItemWrapper,
-    ItemWrapperContainer,
-    OutletContainer,
-    OutletWrapper
+  ContainerStyled,
+  ContentContainer,
+  ItemWrapper,
+  ItemWrapperMessage,
+  ItemWrapperContainer,
+  ItemWrapperContainerMessage,
+  OutletContainer,
+  OutletWrapper,
+  OutletWrapperMessage,
 } from "./LayoutStyles";
 
 import { RegistrationPage } from "../pages/RegistrationPage";
 import {
-    setUserId,
-    fetchPostsByUserId,
-    fetchExplorePosts,
-    setPage,
-    setUserData, setUserPostsClear, setPageZero,fetchData
+  setUserId,
+  fetchPostsByUserId,
+  fetchExplorePosts,
+  setPage,
+  setUserData,
+  setUserPostsClear,
+  setPageZero,
+  fetchData,
 } from "../store/actions";
 import { decodeToken } from "./Posts/decodeToken";
-import { BirthdateForm } from "./LoginModal/BirthdateForm";
-import {apiUrl} from "../apiConfig";
+import { apiUrl } from "../apiConfig";
 
-export const ScrollContext = React.createContext(() => {
-});
+export const ScrollContext = React.createContext(() => {});
 
 export function Layout() {
-    const navigate = useNavigate();
-    const userToken = JSON.parse(localStorage.getItem("userToken")) || JSON.parse(sessionStorage.getItem("userToken"));
-    const userBirthdateGoogle = useSelector(state => state.saveUserToken.userBirthdayFlag);
-    const page = useSelector(state => state.pageCount.page);
-    const dispatch = useDispatch();
-    let location = useLocation();
-    const [allPostsLoaded, setAllPostsLoaded] = useState(false);
-    const userId = useSelector(state => state.userData.userData.userId);
-    const [loadingPosts, setLoadingPosts] = useState(false);
+  const navigate = useNavigate();
+  const userToken =
+    JSON.parse(localStorage.getItem("userToken")) ||
+    JSON.parse(sessionStorage.getItem("userToken"));
+  const userBirthdateGoogle = useSelector(
+    (state) => state.saveUserToken.userBirthdayFlag
+  );
+  const page = useSelector((state) => state.pageCount.page);
+  const dispatch = useDispatch();
+  let location = useLocation();
+  const [allPostsLoaded, setAllPostsLoaded] = useState(false);
+  const userId = useSelector((state) => state.userData.userData.userId);
+  const [loadingPosts, setLoadingPosts] = useState(false);
 
-    useEffect(() => {
-        if (userToken && userBirthdateGoogle === "true" || userToken && userBirthdateGoogle === "false") {
-            navigate("/home");
+  useEffect(() => {
+    if (
+      userToken &&
+      (userBirthdateGoogle === "true" || userBirthdateGoogle === "false")
+    ) {
+      navigate("/home");
+    }
+  }, []);
+
+  const fetchPosts = async (page) => {
+    const decodedToken = decodeToken();
+    if (decodedToken) {
+      const userId = decodedToken.sub;
+      dispatch(setUserId(userId));
+      // dispatch(fetchPostsByUserId(userId, page));
+      await dispatch(fetchData(userId));
+    }
+  };
+
+  useEffect(() => {
+    // setPageZero();
+    // setUserPostsClear([]);
+    dispatch(fetchData(userId));
+    // fetchPosts(page);
+  }, []);
+
+  const handleParentScroll = useCallback(
+    async (event) => {
+      const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
+      if (
+        scrollHeight - scrollTop <= clientHeight + 20 &&
+        !allPostsLoaded &&
+        !loadingPosts
+      ) {
+        setLoadingPosts(true);
+        let newPosts;
+        const page2 = page + 1;
+        console.log(page);
+        if (location.pathname === "/explore") {
+          newPosts = await dispatch(fetchExplorePosts(userId, page2));
+          console.log("newPostsExplore", newPosts);
+        } else if (location.pathname === "/home") {
+          console.log(page);
+          newPosts = await dispatch(fetchPostsByUserId(userId, page2));
+          console.log("newPostsHome", newPosts);
         }
-    }, []);
-
-    const fetchPosts = async (page) => {
-        const decodedToken = decodeToken();
-        if (decodedToken) {
-            const userId = decodedToken.sub;
-            dispatch(setUserId(userId));
-             // dispatch(fetchPostsByUserId(userId, page));
-            await dispatch(fetchData(userId));
+        if (newPosts && newPosts.length === 0) {
+          console.log("All posts loaded, stopping further fetches");
+          setAllPostsLoaded(true);
+          setLoadingPosts(false);
+        } else {
+          dispatch(setPage(page2));
+          setLoadingPosts(false);
         }
-    };
+      }
+    },
+    [dispatch, location.pathname, page, fetchPosts, allPostsLoaded]
+  );
 
-    useEffect(() => {
-        // setPageZero();
-        // setUserPostsClear([]);
-        dispatch(fetchData(userId));
-        // fetchPosts(page);
-    }, []);
-
-
-
-    const handleParentScroll = useCallback(async (event) => {
-        const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
-        if (scrollHeight - scrollTop <= clientHeight + 20 && !allPostsLoaded && !loadingPosts) {
-            setLoadingPosts(true);
-            let newPosts;
-            const page2 = page + 1;
-            console.log(page)
-            if (location.pathname === "/explore") {
-                newPosts = await dispatch(fetchExplorePosts(userId, page2));
-                console.log("newPostsExplore",newPosts )
-            } else if (location.pathname === "/home") {
-                console.log(page);
-                newPosts = await dispatch(fetchPostsByUserId(userId, page2));
-                console.log("newPostsHome", newPosts )
-            }
-            if (newPosts && newPosts.length === 0) {
-                console.log('All posts loaded, stopping further fetches');
-                setAllPostsLoaded(true);
-                setLoadingPosts(false);
-            } else {
-                dispatch(setPage(page2));
-                setLoadingPosts(false);
-            }
-        }
-    }, [dispatch, location.pathname, page, fetchPosts, allPostsLoaded]);
-
-
-
-    return (
-        <ScrollContext.Provider value={{ handleScroll: handleParentScroll, loadingPosts: loadingPosts }}>
-            {userToken ? (
-                <Container maxWidth="false" sx={ContainerStyled}>
-                    <div style={ContentContainer} onScroll={handleParentScroll}>
-                        <SideBar/>
-                        <div style={ItemWrapper}>
-                            <div style={ItemWrapperContainer}>
-                                <HeaderInformation/>
-                                <div style={OutletContainer}>
-                                    <div style={OutletWrapper}>
-                                        <Outlet/>
-                                    </div>
-                                </div>
-                            </div>
+  return (
+    <ScrollContext.Provider
+      value={{ handleScroll: handleParentScroll, loadingPosts: loadingPosts }}
+    >
+      {userToken ? (
+        <Container maxWidth="false" sx={ContainerStyled}>
+          
+          <div style={ContentContainer} onScroll={handleParentScroll}>
+            <SideBar />
+            {!location.pathname.includes("/messages") &&
+            <>
+            <div style={ItemWrapper}>
+              <div style={ItemWrapperContainer}>
+                <HeaderInformation />
+                <div style={OutletContainer}>
+                  <div style={OutletWrapper}>
+                    <Outlet />
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* {userBirthdateGoogle ? null : <BirthdateForm />} */}
+            <UsersSearch /></>}
+            {location.pathname.includes("/messages") &&
+                <>
+                    <div style={ItemWrapperMessage}>
+                    <div style={ItemWrapperContainerMessage}>
+                        <HeaderInformation />
+                        <div style={OutletContainer}>
+                        <div style={OutletWrapperMessage}>
+                            <Outlet />
                         </div>
-                        {userBirthdateGoogle ? null : <BirthdateForm/>}
-                        <UsersSearch/>
+                        </div>
                     </div>
-                </Container>) : (<RegistrationPage/>)
-            }
-        </ScrollContext.Provider>
-    );
-};
+                    </div>
+                </>}
+          </div>
+        </Container>
+      ) : (
+        <RegistrationPage />
+      )}
+    </ScrollContext.Provider>
+  );
+}
---
+++ social-network-ui/src/pages/Message.js
@@ -0,0 +1,272 @@
+import React, { useEffect, useContext, useState } from "react";
+import { PostsDisplaying } from "../components/Posts/PostsDisplaying";
+import {
+    fetchExplorePosts, setPage, setPageZero,
+} from "../store/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { ScrollContext } from "../components/Layout.js";
+import SearchIcon from '@mui/icons-material/Search';
+
+
+
+export function Message() {
+    function generateBlueMessage(text, timestampText, profileImagePath, username) {
+        return (
+          <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-end",
+            marginBottom: "20px",
+          }}>
+            <div style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: "10px",
+            }}>
+              <div style={{
+                color: "gray",
+                marginRight: "5px",
+              }}>{timestampText}</div>
+              <div style={{
+                color: "gray",
+                marginRight: "5px",
+              }}>·</div>
+              <div style={{
+                color: "gray",
+                marginRight: "5px",
+              }}>@{username}</div>
+              <div style={{
+                fontWeight: "bold",
+                marginRight: "5px",
+              }}>{username}</div>
+              <img src={profileImagePath} alt="Profile Picture" style={{
+                width: "30px",
+                height: "30px",
+                borderRadius: "50%",
+                marginRight: "10px",
+              }} />
+            </div>
+            <div style={{
+              background: "blue",
+              color: "white",
+              padding: "10px",
+              borderRadius: "10px",
+              maxWidth: "400px",
+              textAlign: "right",
+            }}>
+              <div style={{
+                fontSize: "14px",
+                lineHeight: "18px",
+              }}>
+                {text}
+              </div>
+            </div>
+          </div>
+        );
+      }
+
+      function generateWhiteMessage(text, timestampText, profileImagePath, username) {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              marginBottom: "20px",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                marginBottom: "10px",
+              }}
+            >
+              <img
+                src={profileImagePath}
+                alt="Profile Picture"
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  borderRadius: "50%",
+                  marginRight: "10px",
+                }}
+              />
+              <div style={{ fontWeight: "bold", marginRight: "5px" }}>
+                {username}
+              </div>
+              <div style={{ color: "gray", marginRight: "5px" }}>@{username}</div>
+              <div style={{ color: "gray", marginRight: "5px" }}>·</div>
+              <div style={{ color: "gray" }}>{timestampText}</div>
+            </div>
+            <div
+              style={{
+                background: "#f5f8fa",
+                padding: "10px",
+                borderRadius: "10px",
+                maxWidth: "400px",
+              }}
+            >
+              <div
+                style={{
+                  fontSize: "14px",
+                  lineHeight: "18px",
+                  color: "#14171a",
+                }}
+              >
+                {text}
+              </div>
+            </div>
+          </div>
+        );
+      }
+
+
+      const inboxContainerStyle = {
+        width: "100%",
+        margin: "0 auto",
+      };
+
+      const messageContainerStyle = {
+        boxSizing: "border-box",
+        display: "flex",
+        padding: "10px",
+        borderRadius: "8px",
+        backgroundColor: "#F5F8FA",
+        marginBottom: "10px",
+        alignItems: "center"
+      };
+      
+      const avatarStyle = {
+        width: "40px",
+        height: "40px",
+        borderRadius: "50%",
+        marginRight: "10px",
+      };
+      
+      const contentStyle = {
+        flex: "1",
+        height: "40px",
+        overflow: "hidden",
+      };
+      
+      const senderStyle = {
+        fontWeight: "bold",
+        marginBottom: "5px",
+      };
+      
+      const messageStyle = {
+        fontSize: "14px",
+      };
+
+      const dateStyle = {
+        marginLeft: "auto",
+        fontSize: "12px",
+        color: "#657786",
+      };
+      
+      function InboxMessage(sender, message, date) {
+        return (
+          <div style={messageContainerStyle}>
+            <img src="avatar.jpg" alt="Avatar" style={avatarStyle} />
+            <div style={contentStyle}>
+              <div style={senderStyle}>{sender}</div>
+              <div style={messageStyle}>{message}</div>
+            </div>
+            <div style={dateStyle}>{date}</div>
+          </div>
+        );
+      }
+      
+      const sortContainerStyle = {
+        display: "flex",
+        alignItems: "center",
+        padding: "10px",
+        marginBottom: "10px",
+      };
+      
+      const searchInputContainerStyle = {
+        position: "relative",
+        flex: "1",
+      };
+      
+      const searchIconStyle = {
+        position: "absolute",
+        top: "50%",
+        right: "8px",
+        transform: "translateY(-50%)",
+        color: "#657786",
+      };
+      
+      const searchInputStyle = {
+        boxSizing: "border-box",
+        width: "100%",
+        border: "none",
+        borderRadius: "20px",
+        padding: "8px 12px",
+        paddingLeft: "32px",
+        backgroundColor: "#E6ECF0",
+        color: "#14171A",
+        fontSize: "14px",
+      };
+      
+      function SortBlock() {
+        return (
+          <div style={sortContainerStyle}>
+            <div style={searchInputContainerStyle}>
+              <input type="text" placeholder="Search" style={searchInputStyle} />
+              <SearchIcon style={searchIconStyle} />
+            </div>
+          </div>
+        );
+      }
+      
+      
+    return (
+        <div style={{display: "flex", paddingBottom: "20px",}}>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                overflowY: "scroll",
+                maxHeight: "calc(100vh - 120px)",
+                padding: "20px 20px",
+                boxSizing: "border-box",
+                width: "600px",
+            }}>
+              {SortBlock()}
+                <div style={inboxContainerStyle}>
+                
+                  {/* {messages.map((msg, index) => ( */}
+                    {/* <InboxMessage key={index} sender={msg.sender} message={msg.message} /> */}
+                    {InboxMessage("John Doe", "Hello!", "June 9, 2023")}
+                    {InboxMessage("Jane Smith", "Just wanted to say hello!", "June 10, 2023")}
+                  {/* ))} */}
+                </div>
+                </div>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "column-reverse",
+                    overflowY: "scroll",
+                    maxHeight: "calc(100vh - 120px)",
+                    padding: "20px 20px",
+                    boxSizing: "border-box",
+                    borderTop: "1px solid rgba(0, 0, 0, 0.1)",
+                    width: "600px",
+                }}>
+                    {generateBlueMessage("1Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryls standard dummy text ever since the 1500s.", "Timestamp", "profile-picture.jpg", "Username")}
+                    {generateBlueMessage("3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryls standard dummy text ever since the 1500s.", "Timestamp", "profile-picture.jpg", "Username")}
+                    {generateBlueMessage("3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryls standard dummy text ever since the 1500s.", "Timestamp", "profile-picture.jpg", "Username")}
+                    {generateBlueMessage("3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryls standard dummy text ever since the 1500s.", "Timestamp", "profile-picture.jpg", "Username")}
+                    {generateBlueMessage("3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryls standard dummy text ever since the 1500s.", "Timestamp", "profile-picture.jpg", "Username")}
+                    {generateBlueMessage("3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryls standard dummy text ever since the 1500s.", "Timestamp", "profile-picture.jpg", "Username")}
+                    {generateBlueMessage("3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryls standard dummy text ever since the 1500s.", "Timestamp", "profile-picture.jpg", "Username")}
+                    {generateBlueMessage("3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryls standard dummy text ever since the 1500s.", "Timestamp", "profile-picture.jpg", "Username")}
+                    {generateBlueMessage("3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryls standard dummy text ever since the 1500s.", "Timestamp", "profile-picture.jpg", "Username")}
+                    {generateWhiteMessage("1Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "Timestamp","profile-picture.jpg", "Username")}
+                </div>
+
+        </div>
+    );
+}
