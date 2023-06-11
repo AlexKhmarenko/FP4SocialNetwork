4b8fe18d83159bbd1832718190dfce038f177720
Andrii Horbenko <andrewhorbenko@gmail.com>
2023-06-10 20:46:39 -0400

    US 43 add api to request mapping

--- social_network_api/src/main/java/com/danit/socialnetwork/rest/PostRestController.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/rest/PostRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,13 +23,14 @@ import java.util.List;
 @Log4j2
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class PostRestController {
   private final PostService postService;
 
 
   /*Method returns  all posts from users that a user follows by his id
    * if userId is empty returns all posts descending by order based on created datetime*/
-  @GetMapping(path = "/api/posts")
+  @GetMapping(path = "/posts")
   public List<PostDtoResponse> getAllPostsFromFollowing(@RequestParam(name = "userId",
       defaultValue = "0") Integer userFollowingId, @RequestParam(name = "page", defaultValue = "0") Integer page) {
     if (userFollowingId == 0) {
@@ -38,7 +40,7 @@ public class PostRestController {
   }
 
   /*Method returns  all posts from users*/
-  @GetMapping(path = "/api/posts/explorer")
+  @GetMapping(path = "/posts/explorer")
   public List<PostDtoResponse> getAllPostsWithShowingRepostByUserId(@RequestParam(name = "userId",
       defaultValue = "0") Integer userId, @RequestParam(name = "page", defaultValue = "0") Integer page) {
     if (userId == 0) {
@@ -49,14 +51,14 @@ public class PostRestController {
 
 
   /*Method save a new post*/
-  @PostMapping(path = "/api/posts")
+  @PostMapping(path = "/posts")
   public ResponseEntity<PostDtoResponse> addPost(@Valid @RequestBody PostDtoSave thePostDtoSave) {
     Post dbPost = postService.savePost(thePostDtoSave);
     return new ResponseEntity<>(PostDtoResponse.from(dbPost), HttpStatus.CREATED);
   }
 
   /*Method returns all posts done by user*/
-  @GetMapping(path = "/api/posts/{userId}")
+  @GetMapping(path = "/posts/{userId}")
   public List<PostDtoResponse> getAllOwnPosts(@PathVariable("userId") Integer userId,
                                               @RequestParam(name = "page", defaultValue = "0")
                                               Integer page) {
@@ -66,7 +68,7 @@ public class PostRestController {
 
 
   /*Method returns all posts liked by user*/
-  @GetMapping(path = "/api/posts/liked/{userId}")
+  @GetMapping(path = "/posts/liked/{userId}")
   public List<PostDtoResponse> getAllLikedPosts(@PathVariable("userId") Integer userId,
                                                 @RequestParam(name = "page", defaultValue = "0")
                                                 Integer page) {
@@ -75,7 +77,7 @@ public class PostRestController {
 
   /*Method returns all posts and reposts in descending order by time when
    they were posted (for own posts) and reposted (for reposts) by user*/
-  @GetMapping("/api/posts/reposts")
+  @GetMapping("/posts/reposts")
   public List<PostDtoResponse> getAllPostsAndRepostsByUserId(@RequestParam(name = "userId", defaultValue = "0")
                                                              Integer userId,
                                                              @RequestParam(name = "page", defaultValue = "0")
