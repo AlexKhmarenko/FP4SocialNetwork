ec98f276f3df2ba1ba5aa4fc12692b3d75f3eb3d
BukanNadya <118114796+BukanNadya@users.noreply.github.com>
2023-06-11 23:16:36 +0300

    [US46] Correct validation for address

--- social_network_api/src/main/java/com/danit/socialnetwork/dto/user/EditingDtoRequest.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/dto/user/EditingDtoRequest.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.Size;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
