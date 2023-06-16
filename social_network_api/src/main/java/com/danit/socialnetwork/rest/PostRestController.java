724f84d892a824316bdf8ea10fa2c0d57cbaf15f
Andrii Horbenko <andrewhorbenko@gmail.com>
2023-06-16 20:33:23 +0300

    US 22.12 remove code smell

--- social_network_api/src/main/java/com/danit/socialnetwork/rest/PostRestController.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/rest/PostRestController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Positive;
 import java.util.ArrayList;
 import java.util.List;
