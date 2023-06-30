fffe909c2342b26d844145505c060301b443f093
Anastasiia Myelnykova <slonotop2103@gmail.com>
2023-06-30 17:20:38 +0300

    add e2e tests to sidebar

--- e2e/tests/capitweet.test.js
+++ e2e/tests/capitweet.test.js
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "../base";
+import { describe, test, expect, beforeEach } from "../base";
 import { BASE_API_URL } from "../utils/constants";
 
 describe("404 Not Found Page", () => {
@@ -23,21 +23,36 @@ describe("Home Page", () => {
     test("Add post success", async ({ commonActions, homePage, page }) => {
         await commonActions.login();
         await homePage.openHomePage();
-        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await page.waitForLoadState();
         await homePage.verifyAddPost();
     });
 
     test("Get explore route success", async ({ commonActions, homePage, sideBar, page }) => {
         await commonActions.login();
         await homePage.openHomePage();
-        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await page.waitForLoadState();
         await sideBar.explorePageOpen();
+    });
+
+    test("Get notification route success", async ({ commonActions, homePage, sideBar, page }) => {
+        await commonActions.login();
+        await homePage.openHomePage();
+        await page.waitForLoadState();
+        await sideBar.notificationsPageOpen();
+    });
+
+    test("Get message route success", async ({ commonActions, homePage, sideBar, page }) => {
+        await commonActions.login();
+        await homePage.openHomePage();
+        await page.waitForLoadState();
+        await sideBar.messagePageOpen();
     });
 
     test("Add like success", async ({ commonActions, homePage, page }) => {
         await commonActions.login();
         await homePage.openHomePage();
-        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await page.waitForLoadState();
+        // await page.pause();
         await homePage.verifyAddPost();
         await homePage.verifyAddLikeHandle();
     });
