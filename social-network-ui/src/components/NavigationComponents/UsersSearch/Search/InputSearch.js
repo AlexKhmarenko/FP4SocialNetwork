3e22698f684d50fa4e5061c4b8ff592bf69efbfa
Sayonara <Alekss3228@gmail.com>
2023-07-03 03:48:30 +0300

    Adaptive for dark theme for user search

--- social-network-ui/src/components/NavigationComponents/UsersSearch/Search/InputSearch.js
+++ social-network-ui/src/components/NavigationComponents/UsersSearch/Search/InputSearch.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import {TextField, Autocomplete, Typography, Grid, Avatar, Box} from "@mui/material";
-import {DarkPopper, DarkUserSearchTextField, UserSearchTextField} from "../../NavigationStyles";
+import {DarkUserSearchTextField, UserSearchTextField} from "../../NavigationStyles";
 import {useDispatch, useSelector} from "react-redux";
 import {DeleteUsersSuccess, setSearchData, setSearchId} from "../../../../store/actions";
 import {useNavigate} from "react-router-dom";
 import {apiUrl} from "../../../../apiConfig";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
-import {DarkPaperStyles, PaperStyles} from "../popularPeopleSidebarStyles";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export const InputSearch = ({ ...props }) => {
     const users = useSelector(state => state.usersSearch.users)
@@ -77,8 +77,33 @@ export const InputSearch = ({ ...props }) => {
         }
     }, [userId]);
 
+    const paperStyle = createTheme({
+        components: {
+            // Name of the component
+            MuiAutocomplete: {
+                styleOverrides: {
+                    // Name of the slot
+                    noOptions: {
+                        // Some CSS
+                        color: darkMode ? "rgb(247, 249, 249)" : "#000000",
+                        background: darkMode ? "rgb(39, 51, 64)" : "#ffffff",
+                    },
+                    paper: {
+                        background: darkMode ? "rgb(39, 51, 64)" : "#ffffff",
+                    },
+                    listbox: {
+                        "&& .Mui-focused": {
+                                    backgroundColor: darkMode ? "rgba(247, 249, 249, 0.1)" : "rgba(0, 0, 0, 0.1)",
+                                },
+                    },
+                },
+            },
+        },
+    });
+
 
     return (
+        <ThemeProvider theme={paperStyle}>
             <Autocomplete
                 getOptionLabel={(option) => option.username}
                 filterOptions={(x) => x}
@@ -113,8 +138,11 @@ export const InputSearch = ({ ...props }) => {
                 )}
                     renderOption={(props, option) => {
 
+
                     return (
-                        <li {...props} key={option.userId} style={DarkPopper}>
+                        <li {...props} key={option.userId}
+
+                        >
                             <Grid container alignItems="center" onClick={() => {
                                 dispatch(setSearchId(String(option.userId)))
                                 navigate("/view")
@@ -125,11 +153,14 @@ export const InputSearch = ({ ...props }) => {
                                 <Grid item sx={{ width: 'calc(100% - 44px)', wordWrap: 'break-word' }}>
                                         <Box
                                             component="span"
-                                            sx={{ fontWeight: 'bold' }}
+                                            sx={{
+                                                fontWeight: 'bold',
+                                                color: darkMode ? "rgb(247, 249, 249)" : "rgba(0, 0, 0, 0.6)"
+                                            }}
                                         >
                                             {option.name}
                                         </Box>
-                                    <Typography variant="body2" color="text.secondary">
+                                    <Typography variant="body2" color="text.secondary" sx={{color: darkMode ? "rgb(247, 249, 249)" : "rgba(0, 0, 0, 0.6)"}}>
                                         @{option.username}
                                     </Typography>
                                 </Grid>
@@ -138,5 +169,21 @@ export const InputSearch = ({ ...props }) => {
                     );
                 }}
             />
+        </ThemeProvider>
     )
 }
+
+// option: {
+//     "&:hover": {
+//         backgroundColor: "rgba(247, 249, 249, 0.1)"
+//     },
+//     "&:focus-within": {
+//         backgroundColor: "rgba(247, 249, 249, 0.1)",
+//     },
+//     "& .Mui-focused": {
+//         backgroundColor: "rgba(247, 249, 249, 0.1)",
+//     },
+//     focused: {
+//         backgroundColor: "rgba(247, 249, 249, 0.1)"
+//     }
+// },
