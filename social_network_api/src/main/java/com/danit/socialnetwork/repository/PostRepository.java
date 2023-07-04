733f0156b2a7fdd6bffa428109153fd33a4f19c4
Andrii Horbenko <andrewhorbenko@gmail.com>
2023-07-04 08:59:43 +0300

    US 49.1 remove bugs

--- social_network_api/src/main/java/com/danit/socialnetwork/repository/PostRepository.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package com.danit.socialnetwork.repository;
 
 import com.danit.socialnetwork.model.Post;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
