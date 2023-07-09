298b6a871e7c5fbd623ec63ec3d7a7305d83949a
BukanNadya <118114796+BukanNadya@users.noreply.github.com>
2023-07-09 09:34:23 +0300

    [US56] Correct "postReadMessages" at the WebSocketController. Fix bug.

--- social_network_api/src/main/java/com/danit/socialnetwork/websocket/WebSocketController.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/websocket/WebSocketController.java
@@ -25,7 +25,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
